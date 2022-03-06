d31eb8f95e0b9358b69c74dd1f57b59231bc6913
rsadhamhussain <rsadhamhussain@gmail.com>
2022-03-06 21:02:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,28 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "us-east-2"
+       description = "The AWS region to create things in." 
+       default     = "ap-south-1" 
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+variable "key_name" { 
+    description = " SSH keys to connect to ec2 instance" 
+    default     =  "pythonisbest" 
 }
 
-variable "instance_type" {
-  description = "instance type for ec2"
-  default     =  "t2.micro"
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
 }
 
-variable "security_group" {
-  description = "Name of security group"
-  default     = "my-jenkins-security-group"
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "my-terra-jenkins-security-group" 
 }
 
-variable "tag_name" {
-  description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
-}
-
-variable "ami_id" {
-  description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-terra-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0851b76e8b1bce90b" 
 }
